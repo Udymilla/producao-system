624b4a2b0b9ec7fb5949e4c562cef1b80fa87e22
Udymilla <udymilla.23111712@alunos.unisagrado.edu.br>
2025-10-07 13:06:43 -0300

    up

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# ⚠️ Substitua 1234 pela sua senha do PostgreSQL
+# ⚙️ Substitua '1234' pela sua senha do PostgreSQL
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost:5432/producao"
 
+# 🔗 Conexão com o banco
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+    
